30ba81199980a73db664df9952e457ba6fa44c24
meins <cedric.madoerin@students.fhnw.ch>
2016-12-20 16:39:02 +0100

    helps find the peak

---
+++ ether-examples/src/main/java/ch/fhnw/ether/examples/tvver/PeakFinder.java
@@ -0,0 +1,74 @@
+package ch.fhnw.ether.examples.tvver;
+
+import java.util.EmptyStackException;
+
+/**
+ * Created by meins on 12.12.2016.
+ *
+ * init
+ * ------
+ * avgPeakOverTime = new AvgPeakOverTime(5,0.003f);
+ *
+ * fill
+ * -----
+ * avgPeakOverTime.push(0.005f)
+ *
+ * check
+ * ------
+ * if(avgPeakOverTime.isPeak(peak))
+ */
+public class PeakFinder {
+    private int top;
+    private float[] storage;
+    private float avgPeak;
+
+
+    PeakFinder(int capacity, float avgPeak){
+        this.avgPeak = avgPeak;
+        if (capacity <= 0){
+            throw new IllegalArgumentException(
+                    "Stack's capacity must be positive");
+        }
+        storage = new float[capacity];
+        init();
+    }
+
+    private void init(){
+        for(int i = 0; i <= storage.length; i ++ ){
+            push(avgPeak);
+        }
+    }
+
+
+    void push(float value){
+//        if (top == storage.length)
+//            throw new EmptyStackException();
+        top = (top+1)%(storage.length-1);
+        storage[top] = value;
+    }
+
+    float getAvg(){
+        if (top == -1)
+            throw new EmptyStackException();
+        float total = 0;
+        for (int i = 0; i < storage.length; i++) {
+            total += storage[i];
+        }
+
+        return total/storage.length;
+    }
+
+    /**
+     * if given peak is higher than the threshhold + the average difference of the last capacity values
+     * @param currentPeak
+     * @return
+     */
+    public boolean isPeak(float currentPeak){
+        if(currentPeak>(avgPeak+(avgPeak-getAvg()))){
+            return true;
+        }
+        return false;
+    }
+
+
+}
